807f59b681af1368b05ad15c0d2009dfa8a9f359
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2023-08-02 20:34:28 -0600

    nummy yummy navbar

---
+++ CSC270_W7-1/Menu.php
@@ -0,0 +1,35 @@
+<?php
+$link = [
+    "link1" => "index.php",
+    "link2" => "page2.php",
+    "link3" => "page3.php",
+]
+?>
+<div class="dropdown"><button class="dropbtn"><a href="index.php" name="Index">Home</a></button></div>
+
+<div class="dropdown">
+    <button class="dropbtn">Genres</button>
+    <div class="dropdown-content">
+        <a href="#">Action</a>
+        <a href="#">Adventure</a>
+        <a href="#">Animation</a>
+        <a href="#">Comedy</a>
+        <a href="#">Drama</a>
+        <a href="#">Fantasy</a>
+        <a href="#">Horror</a>
+        <a href="#">Romance</a>
+    </div>
+</div>
+
+<div class="dropdown"><button class="dropbtn"><a href="#" name="Account">Account</a></button></div>
+
+<div class="dropdown">
+    <button class="dropbtn">Lists</button>
+    <div class="dropdown-content">
+        <a href="#">History</a>
+        <a href="#">Watch Later</a>
+        <a href="#">Recommendations</a>
+        <a href="#">Custom List 1</a>
+        <a href="#">Custom List 2</a>
+    </div>
+</div>
