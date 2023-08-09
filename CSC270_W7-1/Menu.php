606c9652961aec90f18c6ac28153cd942a52b85d
ArturoDuran90 <aduran@student.neumont.edu>
2023-08-09 08:35:39 -0600

    asd

--- CSC270_W7-1/Menu.php
+++ CSC270_W7-1/Menu.php
@@ -5,7 +5,9 @@ $linkTO = [
     "About" => "about.php",
     "Account" => "account.php",
     "Lists" => "list.php",
-    "Preferences" => "preferences.php"
+    "Preferances" => "preferances.php",
+    "Login" => "Login.php",
+    "Signup" => "SignUp.php"
 ]
 ?>
 <?php
@@ -20,36 +22,59 @@ echo'
 <div class="dropdown">
     <button class="dropbtn">Genres</button>
     <div class="dropdown-content">
+        <a href="' . $linkTO["Genre"] . '?genre=action">Action</a>
+        <a href="' . $linkTO["Genre"] . '?genre=adventure">Adventure</a>
         <a href="' . $linkTO["Genre"] . '?genre=animation">Animation</a>
         <a href="' . $linkTO["Genre"] . '?genre=comedy">Comedy</a>
-        <a href="' . $linkTO["Genre"] . '?genre=christmas">Christmas</a>
+        <a href="' . $linkTO["Genre"] . '?genre=drama">Drama</a>
         <a href="' . $linkTO["Genre"] . '?genre=fantasy">Fantasy</a>
         <a href="' . $linkTO["Genre"] . '?genre=horror">Horror</a>
-        <a href="' . $linkTO["Genre"] . '?genre=sci-fi">Sci-Fi</a>
-    </div>
-</div>
-
-<div class="dropdown">
-    <button class="dropbtn">About</button>
-    <div class="dropdown-content">
-        <a href="' . $linkTO["About"] . '?section=location">Locations</a>
-        <a href="' . $linkTO["About"] . '?section=contact">Email Us</a>
-    </div>
-</div>
-
-
-<div class="dropdown">
-    <button class="dropbtn">Lists</button>
-    <div class="dropdown-content">
-        <a href="' . $linkTO["Lists"] . '?list=history">History</a>
-        <a href="' . $linkTO["Lists"] . '?list=watch later">Watch Later</a>
-        <a href="?list=edit lists">Edit Lists</a>
+        <a href="' . $linkTO["Genre"] . '?genre=romance">Romance</a>
     </div>
 </div>
 
 <div class="dropdown">
     <button class="dropbtn">
-        <a href="' . $linkTO["Preferences"] . '?name="Preferences">Preferences</a>
+        <a href="' . $linkTO["Account"] . '" name="Account">Account</a>
     </button>
-</div>'
+</div>
+
+<div class="dropdown">
+    <button class="dropbtn">Lists</button>
+    <div class="dropdown-content">
+        <a href="' . $linkTO["Lists"] . '?list=history">History</a>
+        <a href="' . $linkTO["Lists"] . '?list=wl">Watch Later</a>
+        <a href="' . $linkTO["Lists"] . '?list=recommendations">Recommendations</a>
+        <a href="' . $linkTO["Lists"] . '">Custom List 1</a>
+        <a href="' . $linkTO["Lists"] . '">Custom List 2</a>
+        <a href="editLists.php">Edit Lists</a>
+    </div>
+</div>
+
+<div class="dropdown">
+    <button class="dropbtn">
+        <a href="' . $linkTO["Preferances"] . '" name="Preferances">Preferances</a>
+    </button>
+</div>
+
+
+<div class="dropdown">
+    <button class="dropbtn">About</button>
+    <div class="dropdown-content">
+        <a href="' . $linkTO["About"] . '?section=location" name="Locations">Locations</a>
+        <a href="' . $linkTO["About"] . '?section=contact" name="EmailUs">Email Us</a>
+    </div>
+</div>
+
+<div class="dropdown">
+    <button class="dropbtn">
+        <a href="' . $linkTO["Login"] . '" name="Login">Login</a>
+    </button>
+</div>
+<div class="dropdown">
+    <button class="dropbtn">
+        <a href="' . $linkTO["Signup"] . '" name="Signup">signUp</a>
+    </button>
+</div>
+'
 ?>
